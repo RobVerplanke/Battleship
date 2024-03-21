1d1dc5ec0448754f4cc34d43a5ffeb6cdfedb9b1
Rob <rverplanke@gmail.com>
2024-03-21 16:23:44 +0100

    create modules ship and gameboard

---
+++ src/modules/battleship.js
@@ -0,0 +1,22 @@
+class Ship {
+  constructor() {
+    this.length = null;
+    this.hit = null;
+    this.sunk = false;
+  }
+
+  // increases the number of ‘hits’ in a ship.
+
+  //   hit() {
+  //     this.hit++;
+  //   }
+
+  // isSunk() {
+  // calculates whether a ship is considered sunk based on its length
+  // and the number of hits it has received.
+  // }
+}
+
+const ship = new Ship();
+
+module.exports = ship;
---
+++ src/modules/gameboard.js
@@ -0,0 +1,18 @@
+class Gameboard {
+  constructor() {
+    this.missedAttacks = null;
+    this.allShipsSunk = false;
+  }
+
+  // Determines whether or not the attack hit a ship
+
+  // receiveAttack() {
+  // takes a pair of coordinates, determines whether or not the attack hit a ship
+  // and then sends the ‘hit’ function to the correct ship, or records the coordinates
+  // of the missed shot.
+  //   }
+}
+
+const gameboard = new Gameboard();
+
+module.exports = gameboard;
