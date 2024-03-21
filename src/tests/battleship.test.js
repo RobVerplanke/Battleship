2d3ddd4c42dc365a55bd3d994a73354252d6a7d8
Rob <rverplanke@gmail.com>
2024-03-21 16:24:16 +0100

    create tests for ship and gameboard modules

---
+++ src/tests/battleship.test.js
@@ -0,0 +1,9 @@
+/* global describe, it, expect */
+
+const ship = require('../modules/battleship.js');
+
+describe('ship', () => {
+  it('should Eexist', () => {
+    expect(ship).toBeDefined();
+  });
+});
---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,9 @@
+/* global describe, it, expect */
+
+const gameboard = require('../modules/gameboard.js');
+
+describe('gameboard', () => {
+  it('should exist', () => {
+    expect(gameboard).toBeDefined();
+  });
+});
