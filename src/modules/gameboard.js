6c7c89715d0e5b1dc4d3a49fd16102233f2282b7
Rob <rverplanke@gmail.com>
2024-03-24 16:47:25 +0100

    finish placeShip method with tests

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -21,19 +21,41 @@ class Gameboard {
     }
   }
 
-  // Validates input coordinates and ship size
+  // Validates coordinate, ship size and orientation arguments
   validateInput(axisX, axisY, shipSize, orientation) {
-    if (axisX === undefined || axisY === undefined || shipSize === undefined) throw new Error('Input is empty'); // Check for empty input
+    if (axisX === undefined || axisY === undefined || shipSize === undefined) throw new Error('Input is incomplete'); // Check for empty input
     if (axisX < 0 || axisX >= this.boardSize || axisY < 0 || axisY >= this.boardSize) throw new Error('Invalid coordinate'); // Check for invalid coordinates
+    if (typeof axisX !== 'number' || typeof axisY !== 'number' || typeof shipSize !== 'number') throw new Error('Input is not a number'); // Check for valid datatype
     if (shipSize < 2 || shipSize > 5) throw new Error('Invalid ship size'); // Check for invalid ship size
-    if (orientation !== 'horizontal' && orientation !== 'vertical') throw new Error('Invalid orientation');
+    if (orientation !== 'horizontal' && orientation !== 'vertical') throw new Error('Invalid orientation'); // Check for valid orientation
+  }
+
+  // Checks if ship can be placed within the board boundries
+  checkBoardBoundries(axisX, axisY, shipSize, orientation) {
+    if (orientation === 'horizontal' && axisX + shipSize > this.boardSize) throw new Error('Ship exceeds board limits'); // Exceeds horizontal board boundry
+    if (orientation === 'vertical' && axisY + shipSize > this.boardSize) throw new Error('Ship exceeds board limits'); // Exceeds vertical board boundry
+  }
+
+  // Checks if the ship overlaps another placed ship
+  checkPlacementOverlap(axisX, axisY, shipSize, orientation) {
+    for (let i = 0; i < shipSize; i++) {
+      if (orientation === 'horizontal' && this.grid[axisX + i][axisY] !== 'empty') throw new Error('Ships overlapping'); // Placing the ship horizontally crosses at least one 'not empty' cell
+      if (orientation === 'vertical' && this.grid[axisX][axisY + i] !== 'empty') throw new Error('Ships overlapping'); // Placing the ship vertically crosses at least one 'not empty' cell
+    }
   }
 
   // Adds a new ship instance to the grid at the specified coordinates with the given size
   placeShip(axisX, axisY, shipSize, orientation) {
     try {
+
       // Validate input values
       this.validateInput(axisX, axisY, shipSize, orientation);
+
+      // Prevent that ship is placed outside the board
+      this.checkBoardBoundries(axisX, axisY, shipSize, orientation);
+
+      // Prevent ship to overlap excisting ship
+      this.checkPlacementOverlap(axisX, axisY, shipSize, orientation);
 
       const newShip = new Ship(shipSize);
 
@@ -52,6 +74,15 @@ class Gameboard {
       throw error;
     }
   }
+
+  // receiveAttack() {
+
+  // }
+
+  // Gameboards should have a receiveAttack function that takes a pair of coordinates,
+  // determines whether or not the
+  // attack hit a ship and then sends the ‘hit’ function to the correct ship,
+  // or records the coordinates of the missed shot.
 }
 
 module.exports = Gameboard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -20,96 +20,187 @@ beforeEach(() => {
 describe('Gameboard', () => {
 
   describe('placeShip', () => {
-  });
-
-  it('throws error when input is empty', () => {
-    const gameBoard = new Gameboard();
   
-    expect(() => {
-      gameBoard.placeShip(); // No arguments given
-    }).toThrow('Input is empty');
-  });
-  
-  it('throws error when a coordinate exceeds board boundries', () => {
-    const gameBoard = new Gameboard();
+    it('throws error when input is empty', () => {
+      const gameBoard = new Gameboard();
     
-    expect(() => {
-      gameBoard.placeShip(-1, 2, 1); // X-axis is to low
-      gameBoard.placeShip(1, 12, 1); // Y-axis is to high
-    }).toThrow('Invalid coordinate');
-  });
-  
-  it('throws error when ship size is invalid', () => {
-    const gameBoard = new Gameboard();
-  
-    expect(() => {
-      gameBoard.placeShip(1, 2, 1); // Size is to small
-      gameBoard.placeShip(1, 2, 6); // Size is to large
-    }).toThrow('Invalid ship size');
-  });
-  
-  it('throws error when orientation is empty', () => {
-    const gameBoard = new Gameboard();
+      expect(() => {
+        gameBoard.placeShip(); // No arguments given
+      }).toThrow('Input is incomplete');
+    });
 
-    expect(() => {
-      gameBoard.placeShip(1, 2, 2, ''); // Orientation is empty
-    }).toThrow('Invalid orientation');
-  });
+    it('throws error when no size and orientation are given', () => {
+      const gameBoard = new Gameboard();
 
-  it('throws error when orientation is undefined', () => {
-    const gameBoard = new Gameboard();
-
-    expect(() => {
-      gameBoard.placeShip(1, 2, 2); // Orientation is undefined
-    }).toThrow('Invalid orientation');
-  });
-
-  it('throws error when orientation is invalid', () => {
-    const gameBoard = new Gameboard();
-
-    expect(() => {
-      gameBoard.placeShip(1, 2, 2, 'diagonal'); // Orientation is invalid
-    }).toThrow('Invalid orientation');
-  });
-
-  it('places a ship at the correct coordinates when input is valid', () => {
-    const gameBoard = new Gameboard();
-
-    gameBoard.placeShip(1, 2, 2, 'horizontal'); // (X-axis, Y-axis, size, orientation)
-
-    expect(gameBoard.grid[1][2]).toEqual({ size: 2, hit: null, sunk: false });
-    expect(gameBoard.grid[2][2]).toEqual({ size: 2, hit: null, sunk: false });
-  });
-
-  it('can place a ship vertically', () => {
-    const gameBoard = new Gameboard();
-
-    const axisX = 1;
-    const axisY = 2;
-    const size = 5;
-
-    gameBoard.placeShip(axisX, axisY, size, 'vertical'); // Orientation is vertical
-
-    for (let i = 0; i < size; i++) {
-      expect(gameBoard.grid[axisX][axisY + i]).toEqual({ size: 5, hit: null, sunk: false });
-    }
+      expect(() => {
+        gameBoard.placeShip(1, 2); // Size and orientation are undefined
+      }).toThrow('Input is incomplete');
+    });
     
+    it('throws error when a coordinate exceeds board boundries', () => {
+      const gameBoard = new Gameboard();
+      
+      expect(() => {
+        gameBoard.placeShip(-1, 2, 1); // X-axis is to low
+        gameBoard.placeShip(1, 12, 1); // Y-axis is to high
+      }).toThrow('Invalid coordinate');
+    });
+    
+    it('throws error when coordinate is not a number', () => {
+      const gameBoard = new Gameboard();
+    
+      expect(() => {
+        gameBoard.placeShip('axisX', 'axisY', 5, 'horizontal'); // Coordinate is not a number
+      }).toThrow('Input is not a number');
+    });
+
+    it('throws error when size is not a number', () => {
+      const gameBoard = new Gameboard();
+    
+      expect(() => {
+        gameBoard.placeShip(1, 2, 'size', 'horizontal'); // Ship size is not a number
+      }).toThrow('Input is not a number');
+    });
+        
+    it('throws error when ship size is invalid', () => {
+      const gameBoard = new Gameboard();
+    
+      expect(() => {
+        gameBoard.placeShip(1, 2, 1); // Ship size is to small
+        gameBoard.placeShip(1, 2, 6); // Ship size is to large
+      }).toThrow('Invalid ship size');
+    });
+
+    it('throws error when orientation is undefined', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(1, 2, 2); // Orientation is undefined
+      }).toThrow('Invalid orientation');
+    });
+
+    it('throws error when orientation is empty', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(1, 2, 2, ''); // Orientation is empty
+      }).toThrow('Invalid orientation');
+    });
+
+    it('throws error when orientation is not recognized', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(1, 2, 3, 'diagonal'); // Orientation not recognized
+      }).toThrow('Invalid orientation');
+    });
+
+    it('throws error when orientation is a number', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(1, 2, 3, 4); // Orientation is a number
+      }).toThrow('Invalid orientation');
+    });
+
+    it('can place a ship vertically', () => {
+      const gameBoard = new Gameboard();
+
+      const axisX = 1;
+      const axisY = 2;
+      const size = 5;
+
+      gameBoard.placeShip(axisX, axisY, size, 'vertical'); // Orientation is vertical
+
+      for (let i = 0; i < size; i++) {
+        expect(gameBoard.grid[axisX][axisY + i]).toEqual({ size: 5, hit: null, sunk: false });
+      }
+    });
+
+    it('can place a ship horizontally', () => {
+      const gameBoard = new Gameboard();
+
+      const axisX = 1;
+      const axisY = 2;
+      const size = 5;
+
+      gameBoard.placeShip(axisX, axisY, size, 'horizontal'); // Orientation is horizontal
+
+      for (let i = 0; i < size; i++) {
+        expect(gameBoard.grid[axisX + i][axisY]).toEqual({ size: 5, hit: null, sunk: false });
+      }
+    });
+
+    it('can place a ship vertically at the edge of the board', () => {
+      const gameBoard = new Gameboard();
+
+      const axisX = 9;
+      const axisY = 0;
+      const size = 5;
+
+      gameBoard.placeShip(axisX, axisY, size, 'vertical'); // Orientation is vertical
+
+      for (let i = 0; i < size; i++) {
+        expect(gameBoard.grid[axisX][axisY + i]).toEqual({ size: 5, hit: null, sunk: false });
+      }
+    });
+
+    it('can place a ship horizontally at the edge of the board', () => {
+      const gameBoard = new Gameboard();
+
+      const axisX = 0;
+      const axisY = 0;
+      const size = 5;
+
+      gameBoard.placeShip(axisX, axisY, size, 'horizontal'); // Orientation is horizontal
+
+      for (let i = 0; i < size; i++) {
+        expect(gameBoard.grid[axisX + 1][axisY]).toEqual({ size: 5, hit: null, sunk: false });
+      }
+    });
+    
+    it('throws error if ship is placed outside of board boundries', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(6, 2, 5, 'horizontal'); // Ship exceeds board boundries horizontally
+      }).toThrow('Ship exceeds board limits');
+
+      expect(() => {
+        gameBoard.placeShip(1, 8, 3, 'vertical'); // Ship exceeds board boundries vertically
+      }).toThrow('Ship exceeds board limits');
+    })
+
+    it('throws error if ship is completely overlapping an excisting ship', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(1, 2, 5, 'horizontal');
+        gameBoard.placeShip(1, 2, 5, 'horizontal'); // Ship is completely overlapped
+      }).toThrow('Ships overlapping');
+    })
+    
+    it('throws error if ship partially overlaps an excisting ship', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(0, 0, 3, 'vertical');
+        gameBoard.placeShip(0, 2, 5, 'vertical'); // Partially overlaps other ship
+      }).toThrow('Ships overlapping');
+    })
+
+    it('throws error if ship is crossing an excisting ship', () => {
+      const gameBoard = new Gameboard();
+
+      expect(() => {
+        gameBoard.placeShip(1, 0, 5, 'vertical');
+        gameBoard.placeShip(0, 2, 3, 'horizontal'); // Ship crosses other ship
+      }).toThrow('Ships overlapping');
+    })
   });
 
-  it('can place a ship horizontally', () => {
-    const gameBoard = new Gameboard();
+  describe('receiveAttack', () => {
 
-    const axisX = 1;
-    const axisY = 2;
-    const size = 5;
-
-    gameBoard.placeShip(axisX, axisY, size, 'horizontal'); // Orientation is horizontal
-
-    for (let i = 0; i < size; i++) {
-      expect(gameBoard.grid[axisX + i][axisY]).toEqual({ size: 5, hit: null, sunk: false });
-    }
   });
-
-  // Test if ship is placed within board boundries
 
 });
