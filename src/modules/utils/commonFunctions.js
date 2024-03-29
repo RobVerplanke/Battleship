5ca11302b0d16484120f99197a7084f76ca3c45b
Rob <rverplanke@gmail.com>
2024-03-29 16:17:36 +0100

    group commonly used functions in a module

---
+++ src/modules/utils/commonFunctions.js
@@ -0,0 +1,29 @@
+const Ship = require('../battleship.js');
+
+// Used for creating ship 'parts' when one ship has to be placed over multiple cells
+function shipFactory(shipSize, orientation) {
+  return new Ship(shipSize, orientation);
+}
+
+// Generate random number within gameboard boundaries
+function getRandomInt(boardSize) {
+  return Math.floor(Math.random() * boardSize);
+}
+
+// Adds a sunken ship to the list of all sunken ships
+function validateCellValue(allSunkenShipsList, cellValue) {
+  const newList = allSunkenShipsList;
+
+  if (cellValue instanceof Ship) { // Cell contains (part of) a ship
+    const ship = cellValue;
+    if (ship.isSunk()) newList.add(ship); // Add sunken ship to list
+  }
+
+  return newList;
+}
+
+module.exports = {
+  shipFactory,
+  validateCellValue,
+  getRandomInt,
+};
